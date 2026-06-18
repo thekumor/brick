f9054ed72fdc13a101cc0b132b6fe1c61d04f42b
Sebastian Kumór <thekumor@gmail.com>
2026-06-18 19:08:45 +0200

    Create chars.py
    + Added chars command

---
+++ commands/chars.py
@@ -0,0 +1,44 @@
+# ================================================================
+#
+#	Tells the user how many characters they've typed.
+#
+#	#License: GPL-2.0-only
+#	#Authors: The Kumor
+#
+# =================================================================
+
+from discord import app_commands
+import sqlite3
+import os
+
+async def GetChars(interaction):
+	path = "/var/brick/databases"
+
+	posts = sqlite3.connect(os.path.join(path, str(interaction.guild.id) + ".db"))
+	cursor = posts.cursor()
+
+	cursor.execute(
+		"SELECT 1 FROM users WHERE discord_id = ?",
+		(interaction.user.id,)
+	)
+	exists = cursor.fetchone()
+
+	count = 0
+
+	if exists:
+		cursor.execute(
+			"SELECT char_count FROM users WHERE discord_id = ?",
+			(interaction.user.id,)
+		)
+		chars = cursor.fetchone()
+
+		if chars is None:
+			count = 0
+		else:
+			count = chars[0]
+
+	posts.close()
+
+	await interaction.response.send_message(f"Your character count: {count}", ephemeral = True)
+
+register = app_commands.Command(name="chars", description="Tells how many characters you've typed.", callback = GetChars)
