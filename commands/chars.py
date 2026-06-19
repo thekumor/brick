8cf750c2c795a96b30ea837bd0e99f603e210471
Sebastian Kumór <thekumor@gmail.com>
2026-06-19 17:28:08 +0200

    Update chars.py
    * Updated chars command to work with current database system

--- commands/chars.py
+++ commands/chars.py
@@ -13,35 +13,10 @@
 from discord import app_commands
 import sqlite3
 import os
+import utility.database
 
 async def GetChars(interaction):
-	path = "/var/brick/databases"
-
-	posts = sqlite3.connect(os.path.join(path, str(interaction.guild.id) + ".db"))
-	cursor = posts.cursor()
-
-	cursor.execute(
-		"SELECT 1 FROM users WHERE discord_id = ?",
-		(interaction.user.id,)
-	)
-	exists = cursor.fetchone()
-
-	count = 0
-
-	if exists:
-		cursor.execute(
-			"SELECT char_count FROM users WHERE discord_id = ?",
-			(interaction.user.id,)
-		)
-		chars = cursor.fetchone()
-
-		if chars is None:
-			count = 0
-		else:
-			count = chars[0]
-
-	posts.close()
-
+	count = utility.database.BrickDatabase.GetValue(interaction.guild, "users", "char_count", "discord_id", interaction.user.id) or 0
 	await interaction.response.send_message(f"Your character count: {count}", ephemeral = True)
 
 chars = app_commands.Command(name="chars", description="Tells how many characters you've typed.", callback = GetChars)
