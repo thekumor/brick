8224445d267a42799fdafaef5b85ca023f25e387
Sebastian Kumór <thekumor@gmail.com>
2026-06-19 17:56:08 +0200

    Update chars.py

--- commands/chars.py
+++ commands/chars.py
@@ -16,7 +16,7 @@ import os
 import utility.database
 
 async def GetChars(interaction):
-	count = utility.database.BrickDatabase.GetValue(interaction.guild, "users", "char_count", "discord_id", interaction.user.id) or 0
+	count = utility.database.BrickDatabase.GetValue(interaction.guild, "users", "discord_id", interaction.user.id, "char_count") or 0
 	await interaction.response.send_message(f"Your character count: {count}", ephemeral = True)
 
 chars = app_commands.Command(name="chars", description="Tells how many characters you've typed.", callback = GetChars)
