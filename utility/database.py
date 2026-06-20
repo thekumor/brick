d683a89ae14da1fbbf46e3cd115e497420161c20
Sebastian Kumór <thekumor@gmail.com>
2026-06-20 08:43:42 +0200

    Update database.py

--- utility/database.py
+++ utility/database.py
@@ -70,7 +70,9 @@ class Database:
 				discord_id INTEGER,
 				char_count INTEGER
 			);
-				 
+			""", True)
+
+			connection.Do("""
 			CREATE TABLE IF NOT EXISTS economy(
 				id INTEGER PRIMARY KEY AUTO_INCREMENT,
 				user_id INTEGER,
