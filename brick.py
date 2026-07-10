873d6142160a12ccfecfee8d8d3f4a91f685100c
Sebastian Kumór <thekumor@gmail.com>
2026-07-10 12:21:58 +0200

    Update brick.py

--- brick.py
+++ brick.py
@@ -12,7 +12,7 @@ from discord import app_commands
 import os
 from dotenv import load_dotenv
 import json
-import time
+import datetime
 
 from commands import ping, register, chars, leaderboard, daily, throw
 import utility.database
@@ -75,7 +75,7 @@ class Client(discord.Client):
 				embed.add_field(name = "Link", value = "[goto](" + link + ")")
 				embed.add_field(name = "Content", value = message.content)
 				embed.set_author(name = message.author.name, icon_url = message.author.avatar.url)
-				embed.timestamp = time.localtime()
+				embed.timestamp = datetime.datetime
 				channelObj = await self.fetch_channel(str(channel))
 				if channelObj is not None:
 					await channelObj.send(embed = embed)
