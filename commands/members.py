7c83de7239beae5eab24819dd291931e4a4560d6
Sebastian Kumór <thekumor@gmail.com>
2026-07-13 22:19:16 +0200

    Create members.py
    + Added members (member count) command

---
+++ commands/members.py
@@ -0,0 +1,16 @@
+# ===============================================================
+#
+#	Displays how many people are on the server.
+#
+#	#License: GPL-2.0-only
+#	#Authors: The Kumor
+#
+# ===============================================================
+
+from discord import app_commands, Permissions
+import discord
+
+async def Display(interaction: discord.Interaction, target: discord.Member):
+	await interaction.response.send_message(f"Member count: { interaction.guild.members.count }")
+
+members = app_commands.Command(name = "members", description = "Displays how many people are on the server.", callback = Display)
