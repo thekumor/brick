cc860f08d094c5ff32c168c6eb2b285be3af9dd0
Sebastian Kumór <thekumor@gmail.com>
2026-08-31 15:53:21 +0200

    Removed empty command
    - Removed ai command

--- brick.py
+++ brick.py
@@ -13,7 +13,7 @@ import os
 from dotenv import load_dotenv
 import json
 
-from commands import ping, register, chars, leaderboard, daily, throw, members, ai
+from commands import ping, register, chars, leaderboard, daily, throw, members
 import utility.database
 import utility.locale
 
@@ -47,7 +47,6 @@ class Client(discord.Client):
 		self.tree.add_command(daily.daily)
 		self.tree.add_command(throw.throw)
 		self.tree.add_command(members.members)
-		self.tree.add_command(ai.ai)
 
 		await self.tree.sync()
 
